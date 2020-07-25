620422ab1e887f8c12de1940c98287ee3fcc034f
gemgames <61914748+gemgames@users.noreply.github.com>
2020-07-24 20:04:23 -0400

    Update index.js

--- dev/index.js
+++ dev/index.js
@@ -1,6 +1,7 @@
-Zhedra.init = function(){
+Zhedra.init = function() {
   Zdog.Tetra = Zhedra.Tetra;
   Zdog.Octa = Zhedra.Octa;
   Zdog.Dodeca = Zhedra.Dodeca;
   Zdog.Icosa = Zhedra.Icosa;
+  Zdog.Cube = Zdog.Box;
 };
