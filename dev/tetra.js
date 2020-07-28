9f1b8f069544138c021559cac07d056ed1cc803d
gemgames <61914748+gemgames@users.noreply.github.com>
2020-07-27 23:58:46 -0400

    Update tetra.js

--- dev/tetra.js
+++ dev/tetra.js
@@ -196,14 +196,14 @@ Zhedra.Tetra = class {
   }
   copy(p) {
     let x = new Zhedra.Tetra({
-      addTo: p.addTo ? p.addTo : undefined,
-      translate: p.translate ? p.translate : undefined,
-      rotate: p.rotate ? p.rotate : undefined,
-      scale: p.scale ? p.scale : undefined,
-      color: p.color ? p.color : undefined,
-      fill: p.fill ? p.fill : undefined,
-      stroke: p.stroke ? p.stroke : undefined,
-      colors: p.colors ? p.colors : undefined
+      addTo: p.addTo ? p.addTo : this.addTo,
+      translate: p.translate ? p.translate : this.translate,
+      rotate: p.rotate ? p.rotate : this.rotate,
+      scale: p.scale ? p.scale : this.scale,
+      color: p.color ? p.color : this.color,
+      fill: p.fill ? p.fill : this.fill,
+      stroke: p.stroke ? p.stroke : this.stroke,
+      colors: p.colors ? p.colors : this.colors
     });
   }
 };
